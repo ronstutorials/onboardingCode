216668026dacb8b806a809205e837373b0e6427d
Ron <ronnie@codota.com>
2018-11-18 19:10:05 +0200

    test

--- src/main/java/TopGitHubRepoFinder.java
+++ src/main/java/TopGitHubRepoFinder.java
@@ -8,15 +8,39 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 
-public class Onboardtest
+public class TopGitHubRepoFinder
 {
     public static void main(String[] args) {
-        String output  = getUrlContents("https://api.github.com/search/repositories?q=google&sort=stars&order=desc");
-        parseThisJson(output);
+        String output  = new TopGitHubRepoFinder().getUrlContents("https://api.github.com/search/repositories?q=google&sort=stars&order=desc");
+        new TopGitHubRepoFinder().parseThisJson(output);
 
     }
 
-    private static void parseThisJson  (String input){
+    private String getUrlContents(String theUrl)
+    {
+        StringBuilder content = new StringBuilder();
+        try
+        {
+            URL url = new URL(theUrl);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String line;
+
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                content.append(line + "\n");
+            }
+            bufferedReader.close();
+            urlConnection.disconnect();
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+
+    private void parseThisJson(String input){
         JSONObject o = new JSONObject(input);
         JSONObject currentobject;
         JSONArray items = o.getJSONArray("items");
@@ -25,30 +49,5 @@ public class Onboardtest
             System.out.println("REPO NAME: " + currentobject.getString("full_name") + " \nSTARS: " + currentobject.get("stargazers_count"));
         }
 
-    }
-    private static String getUrlContents(String theUrl)
-    {
-        StringBuilder content = new StringBuilder();
-        try
-        {
-            URL url = new URL(theUrl);
-
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-
-            String line;
-
-            while ((line = bufferedReader.readLine()) != null)
-            {
-                content.append(line + "\n");
-            }
-            bufferedReader.close();
-        }
-        catch(Exception e)
-        {
-            e.printStackTrace();
-        }
-        return content.toString();
     }
 }
